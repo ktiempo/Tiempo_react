f24551bd86ae050e4c1baa438ee62516a8f7d288
ktiempo <160035579+ktiempo@users.noreply.github.com>
2024-05-16 16:56:57 +0800

    react-calcu

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+
+import React, { useEffect } from 'react';
+import './App.css';
+import Calculator from './calculator';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title = 'My Calculator App'; 
+  }, []); 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Tiempo - React </h1>
-      <h3>Tiempo - React </h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
-export default App
+export default App;
